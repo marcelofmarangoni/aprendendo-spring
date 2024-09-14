d39f49ab44fb62843e191646196b9c473fe00f11
Marcelo <marcelofmarangoni@gmail.com>
2024-09-14 12:22:28 -0300

    desenvolvido apo para aprendizado de spring security e spring data

---
+++ src/main/java/com/javanauta/aprendendo_spring/infrastructure/exceptions/ConflictException.java
@@ -0,0 +1,12 @@
+package com.javanauta.aprendendo_spring.infrastructure.exceptions;
+
+public class ConflictException extends RuntimeException{
+
+    public ConflictException(String mensagem){
+        super(mensagem);
+    }
+    public ConflictException(String mensagem, Throwable throwable){
+        super(mensagem);
+    }
+
+}
